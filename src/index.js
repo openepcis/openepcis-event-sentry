aea484d33700867529d0238795367c0454a42044
Akshit Patel <akshit.patel@benelog.com>
2024-04-12 12:50:14 +0530

    GEN-47: create structure for event profile checker module

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * (c) Copyright Reserved OpenEPCIS 2024. All rights reserved.
+ * Use of this material is subject to license.
+ * Copying and unauthorised use of this material strictly prohibited.
+ */
+
+export * from "./utils/eventUtils";
---
+++ src/utils/eventUtils.js
@@ -0,0 +1,5 @@
+/**
+ * (c) Copyright Reserved OpenEPCIS 2024. All rights reserved.
+ * Use of this material is subject to license.
+ * Copying and unauthorised use of this material strictly prohibited.
+ */
